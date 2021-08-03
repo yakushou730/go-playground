2ef6df85984f134aaaf8f5c50b446ae6b1843281
Shou Tseng <yakushou730@gmail.com>
2021-08-03 23:39:43 +0800

    feat: serve gRPC & HTTP in different ports

--- tag-service/main.go
+++ tag-service/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net"
+	"net/http"
 	pb "playground/tag-service/proto"
 	"playground/tag-service/server"
 
@@ -11,16 +13,52 @@ import (
 	"google.golang.org/grpc"
 )
 
-func main() {
+var grpcPort string
+var httpPort string
+
+func init() {
+	flag.StringVar(&grpcPort, "grpc_port", "8001", "gRPC啟動埠編號")
+	flag.StringVar(&httpPort, "http_port", "9001", "HTTP啟動埠編號")
+	flag.Parse()
+}
+
+func RunHttpServer(port string) error {
+	serveMux := http.NewServeMux()
+	serveMux.HandleFunc("/ping",
+		func(w http.ResponseWriter, r *http.Request) {
+			_, _ = w.Write([]byte("pong"))
+		},
+	)
+	return http.ListenAndServe(":"+port, serveMux)
+}
+func RunGrpcServer(port string) error {
 	s := grpc.NewServer()
 	pb.RegisterTagServiceServer(s, server.NewTagServer())
 	reflection.Register(s)
-	lis, err := net.Listen("tcp", ":50051")
+	lis, err := net.Listen("tcp", ":"+port)
 	if err != nil {
-		log.Fatalf("net.Listen err: %v", err)
+		return err
 	}
-	err = s.Serve(lis)
-	if err != nil {
-		log.Fatalf("server.Serve err: %v", err)
+	return s.Serve(lis)
+}
+
+func main() {
+	errs := make(chan error)
+	go func() {
+		err := RunHttpServer(httpPort)
+		if err != nil {
+			errs <- err
+		}
+	}()
+	go func() {
+		err := RunGrpcServer(grpcPort)
+		if err != nil {
+			errs <- err
+		}
+	}()
+
+	select {
+	case err := <-errs:
+		log.Fatalf("Run server err: %v", err)
 	}
 }
